1f9a4a1d29747756444abbe5a4f7e17514705fb1
wskeee <361733529@qq.com>
2017-08-03 16:25:26 +0800

    1、前后台分开做权限控制 2、添加采购商 3、IP是否授权检测

--- environments/dev/backend/config/main-local.php
+++ environments/dev/backend/config/main-local.php
@@ -15,6 +15,22 @@ $config = [
             'class' => 'backend\modules\user\Module',
         ],
     ],
+    'as access' => [
+        'class' => 'wskeee\rbac\components\AccessControl',
+        'allowActions' => [
+            'site/*',
+            'rbac/*',
+            'gii/*',
+            'debug/*',
+            'user/*',
+            'study/*',
+            // The actions listed here will be allowed to everyone including guests.
+            // So, 'admin/*' should not appear here in the production, of course.
+            // But in the earlier stages of your development, you may probably want to
+            // add a lot of actions here until you finally completed setting up rbac,
+            // otherwise you may not even take a first step.
+        ]
+    ],
 ];
 
 if (!YII_ENV_TEST) {
--- environments/prod/backend/config/main-local.php
+++ environments/prod/backend/config/main-local.php
@@ -6,4 +6,28 @@ return [
             'cookieValidationKey' => '',
         ],
     ],
+    'modules' => [
+        'course' => [
+            'class' => 'backend\modules\course\Module',
+        ],
+        'user' => [
+            'class' => 'backend\modules\user\Module',
+        ],
+    ],
+    'as access' => [
+        'class' => 'wskeee\rbac\components\AccessControl',
+        'allowActions' => [
+            'site/*',
+            'rbac/*',
+            'gii/*',
+            'debug/*',
+            'user/*',
+            'study/*',
+            // The actions listed here will be allowed to everyone including guests.
+            // So, 'admin/*' should not appear here in the production, of course.
+            // But in the earlier stages of your development, you may probably want to
+            // add a lot of actions here until you finally completed setting up rbac,
+            // otherwise you may not even take a first step.
+        ]
+    ],
 ];
